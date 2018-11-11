1818c7d440af00e3dfa325a7ef7e3afb9fbf2ac7
Brian Bancroft <hello@brianbancroft.ca>
2018-11-10 20:02:08 -0500

    ✨  Use promise all to collect json objects

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+const { get } = require('axios')
+
 // 1. Get the Stac versions
 
 // 2. Get the root file
@@ -5,3 +8,30 @@
 // 3. Validate the root file
 
 // 4. Recursion!
+
+const stacVersions = [
+  'v0.4.0',
+  'v0.4.1',
+  'v0.5.0',
+  'v0.5.1',
+  'v0.5.2',
+  'v0.6.0',
+]
+const baseUrls = {
+  cdn: {
+    catalog: version => `https://cdn.staclint.com/${version}/catalog.json`,
+    item: version => `https://cdn.staclint.com/${version}/item.json`,
+    collection: version =>
+      `https://cdn.staclint.com/${version}/collection.json`,
+  },
+  // TODO: Figure out how to crunch this into a function
+  // git: 'https://raw.githubusercontent.com/radiantearth/stac-spec/',
+}
+
+const catalogRequests = version => [
+  get(baseUrls.cdn.catalog(version)),
+  get(baseUrls.cdn.item(version)),
+  get(baseUrls.cdn.collection(version)),
+]
+
+Promise.all(catalogRequests('v0.6.0')).then(values => console.log(values))
