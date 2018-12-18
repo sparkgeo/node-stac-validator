15c65765a43fd43ae0c86bd9287682f11eaaa27e
Brian Bancroft <hello@brianbancroft.ca>
2018-12-18 12:37:15 -0500

    🚧   Entry points

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const validateFromJson = ({ collection, item, catalog, dig } = {}) => {}
+
+const validateFromUrl = ({ url, dig } = {}) => {}
+
+module.exports = {
+  validateFromJson,
+  validateFromUrl,
+}
