b6cb988e942870eb671d03cb40d66d643f5e3352
Brian Bancroft <hello@brianbancroft.ca>
2019-03-20 14:27:01 -0400

    🔧  Make webpack target node

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  target: 'node',
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
   },
